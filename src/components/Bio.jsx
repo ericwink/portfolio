92a2870796bd668c39af11e87e097b9b102dfa3c
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-09-15 15:54:15 -0400

    update bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -3,7 +3,7 @@ export default function Bio({ addToFadeDowns }) {
         <section id='bio' ref={addToFadeDowns}>
             <img src="/eric.jpg" alt="" />
             <h2>Who I am</h2>
-            <p>With a 10 year career in the IT solutions industry as a respected leader in various roles like Inside Sales, Senior Management, Training, Warehouse Systems and Logistics, I am utilizing my passion of creative problem solving and knowledge as a developer.</p>
+            <p>With a 10 year career in the IT solutions industry as a respected leader in various roles like Inside Sales, Senior Management, Training, Warehouse Systems and Logistics, I gained invaluable experience in collaboration, problem solving, and mastered rapidly learning new things. As a developer, I am utilizing my skills and my passion for code to build unique user experiences.</p>
             {/* <div className='buttons'>
                 <button className='highlight'>Download Resume</button>
                 <button>Hire Me</button>
