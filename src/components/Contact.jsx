a27a6cb1912d8a615c8fd81e93b3e61bb7240fe5
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-09-15 14:06:15 -0400

    update form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,12 @@ export default function Contact({ addToFadeDowns }) {
         <section id='contact' ref={addToFadeDowns}>
             <img src="/backlitkeyboard.jpg" alt="backlit keyboard" />
             <h2>Contact</h2>
-            <input type="text" name='name' placeholder='Name' />
-            <input type="text" name='email' placeholder='Email' />
-            <input type="text" name='subject' placeholder='Subject' />
-            <textarea name="message" cols="30" rows="8" placeholder='Message'></textarea>
+            <form name='portfolio-contact' method='POST' data-netlify='true'>
+                <input type="text" name='name' placeholder='Name' />
+                <input type="text" name='email' placeholder='Email' />
+                <input type="text" name='subject' placeholder='Subject' />
+                <textarea name="message" cols="30" rows="8" placeholder='Message'></textarea>
+            </form>
         </section>
     )
 }
