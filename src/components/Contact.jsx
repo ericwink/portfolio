60dfb67ea92b6076eb8d3c7fb93ddd805e73ba07
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-09-15 14:20:34 -0400

    update form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,12 +3,17 @@ export default function Contact({ addToFadeDowns }) {
         <section id='contact' ref={addToFadeDowns}>
             <img src="/backlitkeyboard.jpg" alt="backlit keyboard" />
             <h2>Contact</h2>
-            <form name='portfolio-contact' method='POST' data-netlify='true'>
+            <form name='portfolio-contact' method='POST' data-netlify='true' onSubmit='submit'>
+                <input type="hidden" name='hidden' value='portfolio-contact' />
+                <label htmlFor="name">Name</label>
                 <input type="text" name='name' placeholder='Name' />
-                <input type="text" name='email' placeholder='Email' />
+                <label htmlFor="email">Email</label>
+                <input type="email" name='email' placeholder='Email' />
+                <label htmlFor="subject">Subject</label>
                 <input type="text" name='subject' placeholder='Subject' />
+                <label htmlFor="message">Message</label>
                 <textarea name="message" cols="30" rows="8" placeholder='Message'></textarea>
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </section>
     )
