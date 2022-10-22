1c18023cf142696e38079b7790713f8c6381eeaf
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2022-10-22 08:41:07 -0400

    update bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -3,8 +3,7 @@ export default function Bio({ addToFadeDowns }) {
         <section id='bio' ref={addToFadeDowns}>
             <img src="/eric2.jpg" alt="" />
             <h2>Who I am</h2>
-            <p>Over 11 years I built a career in the IT solutions industry as a respected leader in various roles like Inside Sales, Senior Management, Training, Warehouse Systems and Logistics.
-                With 8 years experience as a manager, and 6 years working directly with Executives to help shape business decisions and rework company processes.<br /><br /> As a software engineer I am actively seeking engaging work that will allow me to positively impact on our society. I currently specialize in web and mobile application development.</p>
+            <p>Over 11 years I built a career in the IT solutions industry as a respected leader in various roles like Inside Sales, Senior Management, Training, Warehouse Systems and Logistics. I have 8 years experience as a manager, and 6 years working directly with Executives to help shape business decisions and rework company processes.<br /><br /> As a software engineer I am actively seeking meaningful work that will challenge my abilities and encourage continued growth. Ultimately I would like to engage in work that positively impacts our society. I currently specialize in web and mobile application development.</p>
             {/* <div className='buttons'>
                 <button className='highlight'>Download Resume</button>
                 <button>Hire Me</button>
