bf53142376428dc4b1f91c42f9503f3d973d706d
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-05-01 17:04:48 -0400

    add error and fetch logic

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,25 +6,28 @@ import { fadeUpChild } from '../../utilities/animations'
 
 export default function Contact() {
     const [submitted, setSubmitted] = useState(false)
+    const [error, setError] = useState(true)
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+        setError(false)
         const formData = new FormData(e.target)
 
-        setSubmitted(true)
-
-        // try {
-        //     await fetch("/", {
-        //         method: "POST",
-        //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        //         body: new URLSearchParams(formData).toString(),
-        //     })
-        //     setSubmitted(true)
-        // } catch (error) {
-        //     console.log(error)
-        // }
+        try {
+            await fetch("/", {
+                method: "POST",
+                headers: { "Content-Type": "application/x-www-form-urlencoded" },
+                body: new URLSearchParams(formData).toString(),
+            })
+            setSubmitted(true)
+        } catch (error) {
+            console.log(error)
+            setError(true)
+        }
     }
 
+
+    const errorMessage = <motion.p variants={fadeUpChild} animate='visible' initial='hidden' key='thanks!'>There was an error, please try again!</motion.p>
     const thankYou = <motion.p variants={fadeUpChild} animate='visible' initial='hidden' key='thanks!'>Thanks for reaching out! I'll be in touch soon!</motion.p>
 
     const form = (
@@ -39,6 +42,7 @@ export default function Contact() {
             <AnimatePresence mode='wait'>
                 {submitted ? thankYou : form}
             </AnimatePresence>
+            {error && errorMessage}
         </section>
     )
 }
