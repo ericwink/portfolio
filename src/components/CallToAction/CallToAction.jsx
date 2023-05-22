0c6e5847bfc0bc1d2200c9c4b66be92e29597b40
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-05-22 15:53:24 -0400

    update copy

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -13,11 +13,13 @@ export default function CallToAction() {
             <div className={styles.container}>
                 <h2 className={styles.title}>Let's Build Your Solution</h2>
 
-                <p className={styles.text}>No matter what your business does, you've got a process that wastes your time. You're doing manual work that takes your focus away from what really matters. I can help you find your solution through code.</p>
+                <p className={styles.text}>No matter what your business does, you have a need for a software solution. Whether that means a new website, a customized application, or automating a manual process.</p>
 
                 <p className={styles.text}>During an 11 year career with a global IT soultions provider I implemented several new processes that enhanced the lives of employees, saved time, improved workflow, reduced costs, and increased customer satisfaction and retention.</p>
 
-                <p className={styles.text}>As a Software Engineer, I utilize my years of identifying and solving business needs to build a custom solution that will work for you. Let's get you back on track to focusing on what really matters for your business.</p>
+                <p className={styles.text}>I utilize my years of experience identifying and solving business problems to undertand your unique needs, then build a custom solution that works for you with my knowledge in Software Engineering.</p>
+
+                <p className={styles.text}>Let's get you back on track to focusing on what really matters for your business.</p>
 
                 <p className={styles.testamonial}>"Eric by far has been able to demonstrate his worth through his work and we're extremely happy to discover him. Highly recommend as we'll definitely be an ongoing clients of his." - Taylor W. (Freelance Client)</p>
 
