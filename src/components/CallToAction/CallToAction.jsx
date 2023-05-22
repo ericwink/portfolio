59ca03c657716a85ea484aed5e21cc0fc1abcd80
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-05-22 15:57:34 -0400

    update copy

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -15,9 +15,9 @@ export default function CallToAction() {
 
                 <p className={styles.text}>No matter what your business does, you have a need for a software solution. Whether that means a new website, a customized application, or automating a manual process.</p>
 
-                <p className={styles.text}>During an 11 year career with a global IT soultions provider I implemented several new processes that enhanced the lives of employees, saved time, improved workflow, reduced costs, and increased customer satisfaction and retention.</p>
+                <p className={styles.text}>During an 11 year career with a global IT solutions provider I implemented several new processes that enhanced the lives of employees, saved time, improved workflow, reduced costs, and increased customer satisfaction and retention.</p>
 
-                <p className={styles.text}>I utilize my years of experience identifying and solving business problems to undertand your unique needs, then build a custom solution that works for you with my knowledge in Software Engineering.</p>
+                <p className={styles.text}>I utilize my years of experience identifying and solving business problems to understand your unique needs, then build a custom solution that works for you with my knowledge in Software Engineering.</p>
 
                 <p className={styles.text}>Let's get you back on track to focusing on what really matters for your business.</p>
 
