e3756f19883591e510e89b5feae9c62e16631381
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-05-22 12:59:35 -0400

    updated copy

--- src/components/CallToAction/CallToAction.jsx
+++ src/components/CallToAction/CallToAction.jsx
@@ -12,8 +12,13 @@ export default function CallToAction() {
         <section className={styles.callToAction}>
             <div className={styles.container}>
                 <h2 className={styles.title}>Let's Build Your Solution</h2>
-                <p className={styles.text}>Do you have a business that needs a web presence? A current website that needs to be rebuilt to ensure it works flawlessly across various screen sizes? Or perhaps you need a completely customized full-stack web application to solve a business problem?</p>
-                <p className={styles.text}> I am excited to work together to understand the challenges you are currently facing, and provide you with the best possible solution to meet your needs.</p>
+
+                <p className={styles.text}>No matter what your business does, you've got a process that wastes your time. You're doing manual work that takes your focus away from what really matters. I can help you find your solution through code.</p>
+
+                <p className={styles.text}>During an 11 year career with a global IT soultions provider I implemented several new processes that enhanced the lives of employees, saved time, improved workflow, reduced costs, and increased customer satisfaction and retention.</p>
+
+                <p className={styles.text}>As a Software Engineer, I utilize my years of identifying and solving business needs to build a custom solution that will work for you. Let's get you back on track to focusing on what really matters for your business.</p>
+
                 <p className={styles.testamonial}>"Eric by far has been able to demonstrate his worth through his work and we're extremely happy to discover him. Highly recommend as we'll definitely be an ongoing clients of his." - Taylor W. (Freelance Client)</p>
 
                 {offerCards}
